d990c58feee0e0ba3cb85f776ac8111ff1c4a45c
Nick Murphy <namurphy@cfa.harvard.edu>
2022-03-19 13:40:18 -0400

    Make updates to GenericDecorator

--- plasmapy/utils/decorators/generic.py
+++ plasmapy/utils/decorators/generic.py
@@ -3,16 +3,119 @@ Generic classes for decorators that process arguments based on
 annotations.
 """
 
-__all__ = ["GenericDecorator"]
+__all__ = ["GenericDecorator", "OriginalGenericDecorator"]
 
 import collections
 import functools
 import inspect
 
-from typing import AbstractSet, Any, Callable, Mapping, NoReturn, Optional, Tuple
+from typing import AbstractSet, Any, Callable, Dict, Mapping, NoReturn, Optional, Tuple
 
 
 class GenericDecorator:
+    """
+    A decorator that can be subclassed to perform operations on the
+    positional arguments, keyword arguments, and return values based on
+    annotations.
+
+    Parameters
+    ----------
+    function_to_decorate : callable, positional-only
+        The function being decorated.
+
+    *args_to_decorator
+        The positional arguments provided to the decorator.
+
+    **kwargs_to_decorator
+        The keyword arguments provided to the decorator.
+
+    """
+
+    def __init__(
+        self,
+        function_to_decorate: Optional[Callable] = None,
+        *args_to_decorator,
+        **kwargs_to_decorator,
+    ):
+        self.initialize()
+        self.args_to_decorator = args_to_decorator
+        self.kwargs_to_decorator = kwargs_to_decorator
+        self.function_being_decorated = function_to_decorate
+        print("Ending __init__")
+
+    def initialize(self):
+        print("Initializing GenericDecorator")
+        self._data = {}
+
+    @property
+    def args_to_decorator(self) -> tuple:
+        """The positional arguments provided to the decorator."""
+        print(f"Accessing args_to_decorator: {args_to_decorator}")
+        return self._data["args_to_decorator"]
+
+    @args_to_decorator.setter
+    def args_to_decorator(self, args: dict):
+        print(f"Storing args_to_decorator: {args}")
+        self._data["args_to_decorator"] = args
+
+    @property
+    def kwargs_to_decorator(self):
+        """The keyword arguments provided to the decorator."""
+        print(f"Accessing kwargs_to_decorator: {self._data['kwargs_to_decorator']}")
+        return self._data["kwargs_to_decorator"]
+
+    @kwargs_to_decorator.setter
+    def kwargs_to_decorator(self, kwargs: Dict[str, Any]):
+        print(f"Storing kwargs_to_decorator: {kwargs}")
+        self._data["kwargs_to_decorator"] = kwargs
+
+    @property
+    def function_being_decorated(self) -> Optional[Callable]:
+        """The function being decorated."""
+        print(
+            f"Accessing function_being_decorated: {self._data['function_being_decorated']}"
+        )
+        try:
+            return self._data["function_being_decorated"]
+        except KeyError:
+            return None
+
+    @function_being_decorated.setter
+    def function_being_decorated(self, function_to_decorate):
+        print(f"Setting function_being_decorated: {function_to_decorate}")
+        if function_to_decorate is not None and not callable(function_to_decorate):
+            raise TypeError("The function being decorated is not callable.")
+        self._data["function_being_decorated"] = function_to_decorate
+
+    def __call__(self, *args, **kwargs):
+        """Run decorated function if available, else decorate first arg."""
+
+        if self.function_being_decorated is not None:
+            return self.run_decorated_function(*args, **kwargs)
+
+        if args[1:] or kwargs:
+            raise ValueError("Placeholder exception; not sure if this is needed.")
+
+        self.function_being_decorated = args[0]
+        return self
+
+    def run_decorated_function(self, *args, **kwargs):
+        """
+        Run
+        :meth:`~plasmapy.utils.decorators.generic.GenericDecorator.function_being_decorated`.
+
+        Parameters
+        ----------
+        *args
+            Positional arguments to be passed to the decorated function.
+
+        **kwargs
+            Keyword arguments to be passed to the decorated function
+        """
+        return self.function_being_decorated(*args, **kwargs)
+
+
+class OriginalGenericDecorator:
     @classmethod
     def as_decorator(cls, func: Callable = None, **kwargs_to_decorator):
         print("calling as_decorator")
--- plasmapy/utils/decorators/tests/test_generic.py
+++ plasmapy/utils/decorators/tests/test_generic.py
@@ -1,14 +1,55 @@
 """Tests of generic decorator functionality."""
 
+import pytest
+
 from plasmapy.utils.decorators.generic import GenericDecorator
 
-generic_decorator = GenericDecorator().as_decorator
 
-
-@generic_decorator
-def decorated_function(x, *, y, z=5):
+@GenericDecorator
+def decorated_function_without_decorator_call(x, *, y, z=5):
     return x, y, z
 
 
-def test_decorated_function():
+@GenericDecorator()
+def decorated_function_with_decorator_call(x, *, y, z=5):
+    return x, y, z
+
+
+@pytest.mark.parametrize(
+    "decorated_function",
+    [
+        decorated_function_with_decorator_call,
+        decorated_function_without_decorator_call,
+    ],
+)
+def test_decorated_function(decorated_function):
     assert decorated_function(2, y=3) == (2, 3, 5)
+
+
+@GenericDecorator
+class DecoratedClassWithoutDecoratorCall:
+    def __init__(self):
+        self.x = 1
+
+
+@GenericDecorator()
+class DecoratedClassWithDecoratorCall:
+    def __init__(self):
+        self.x = 1
+
+
+@pytest.mark.parametrize(
+    "DecoratedClass",
+    [
+        DecoratedClassWithoutDecoratorCall,
+        DecoratedClassWithoutDecoratorCall,
+    ],
+)
+def test_decoration_of_classes(DecoratedClass):
+    instance = DecoratedClass()
+    assert instance.x == 1
+
+
+def test_exception():
+    with pytest.raises(TypeError):
+        GenericDecorator(1)
