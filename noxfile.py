f8e9683176ba4a326a633f4ac09cd6312834238e
Nick Murphy <namurphy@cfa.harvard.edu>
2022-04-27 17:35:36 -0400

    Update noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -3,11 +3,11 @@ import nox
 
 @nox.session
 def tests(session):
-    session.install("pytest")
+    session.install("-r", "requirements/tests.txt")
     session.run("pytest")
 
 
 @nox.session
 def lint(session):
-    session.install("flake8")
-    session.run("flake8", "--import-order-style", "google")
+    session.install("-r", "requirements/tests.txt")
+    session.run("flake8")
