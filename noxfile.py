6e17c774fae5e3acae4cce774039e96cdfdd955a
Nick Murphy <namurphy@cfa.harvard.edu>
2022-04-27 18:34:33 -0400

    Update noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -1,13 +1,38 @@
 import nox
 
+python_versions = ("3.8", "3.9", "3.10")
 
-@nox.session
+
+@nox.session(python=python_versions, reuse_venv=True)
 def tests(session):
     session.install("-r", "requirements/tests.txt")
     session.run("pytest")
 
 
 @nox.session
-def lint(session):
+def linters(session):
     session.install("-r", "requirements/tests.txt")
-    session.run("flake8")
+    session.run("flake8", "--bug-report")
+    flake8_options = ["--count", "--show-source", "--statistics"]
+    session.run("flake8", "plasmapy", *flake8_options)
+
+
+@nox.session
+def build_docs(session):
+    session.install("-r", "requirements/docs.txt")
+    session.run(
+        "sphinx-build",
+        "docs",
+        "docs/_build/html",
+        "-W",
+        "--keep-going",
+        "-b",
+        "html",
+        *session.posargs,
+    )
+
+
+@nox.session
+def codespell(session):
+    session.install("codespell")
+    session.run("codespell", ".")
