aebddac742466f4c58ff2e9dfd0961e674676d14
Nick Murphy <namurphy@cfa.harvard.edu>
2022-04-28 13:35:26 -0400

    Update noxfile

--- noxfile.py
+++ noxfile.py
@@ -3,23 +3,24 @@ import nox
 python_versions = ("3.8", "3.9", "3.10")
 
 
-@nox.session(python=python_versions, reuse_venv=True)
+@nox.session(python=python_versions)
 def tests(session):
     session.install("-r", "requirements/tests.txt")
+    session.install(".")
     session.run("pytest")
 
 
 @nox.session
 def linters(session):
     session.install("-r", "requirements/tests.txt")
-    session.run("flake8", "--bug-report")
     flake8_options = ["--count", "--show-source", "--statistics"]
-    session.run("flake8", "plasmapy", *flake8_options)
+    session.run("flake8", "plasmapy", *flake8_options, *session.posargs)
 
 
 @nox.session
 def build_docs(session):
     session.install("-r", "requirements/docs.txt")
+    session.install(".")
     session.run(
         "sphinx-build",
         "docs",
@@ -33,6 +34,24 @@ def build_docs(session):
 
 
 @nox.session
+def build_docs_no_examples(session):
+    session.install("-r", "requirements/docs.txt")
+    session.install(".")
+    session.run(
+        "sphinx-build",
+        "-D",
+        "nbsphinx_execute=never",
+        "docs",
+        "docs/_build/html",
+        "-W",
+        "--keep-going",
+        "-b",
+        "html",
+        *session.posargs,
+    )
+
+
+@nox.session(python="3.10")
 def codespell(session):
     session.install("codespell")
     session.run("codespell", ".")
