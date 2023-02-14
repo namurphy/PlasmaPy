9a16e3e3ba9a9306e3c8a2c015da6f28225e7684
Nick Murphy <namurphy@cfa.harvard.edu>
2023-02-14 10:51:33 -0500

    Update linters and build_docs environments

---
+++ noxfile.py
@@ -0,0 +1,37 @@
+import nox
+
+nox.options.sessions = ["linters", "build_docs"]
+
+python_versions = ("3.9", "3.10")
+newest_python_version = python_versions[-1]
+
+pytest_options = ["--showlocals"]
+
+sphinx_paths = ("docs", "docs/_build/html")
+sphinx_fail_on_warnings = ("-W", "--keep-going")
+sphinx_builder = ("-b", "html", "-n")
+sphinx_opts = sphinx_paths + sphinx_fail_on_warnings + sphinx_builder
+sphinx_skip_notebooks = ("-D", "nbsphinx_execute=never")
+
+post_doc_build_comments = """
+For troubleshooting documentation builds, check out:
+https://docs.plasmapy.org/en/latest/contributing/doc_guide.html#troubleshooting
+"""
+
+
+@nox.session
+def build_docs(session):
+    session.install(".[docs]")
+    session.run(
+        "sphinx-build",
+        *sphinx_opts,
+        *session.posargs,
+    )
+    print(post_doc_build_comments)
+
+
+@nox.session
+def linters(session):
+    session.install(".[tests]")
+    flake8_options = ["--count", "--show-source", "--statistics"]
+    session.run("flake8", "plasmapy", *flake8_options, *session.posargs)
