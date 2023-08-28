d4ca6184dd96bc5cd5c90764c2546da9e18bbbf3
Nick Murphy <namurphy@cfa.harvard.edu>
2023-08-28 18:57:45 -0400

    Minor updates to HarrisSheet & tests

--- plasmapy/plasma/equilibria1d.py
+++ plasmapy/plasma/equilibria1d.py
@@ -11,11 +11,7 @@ from plasmapy.utils.decorators.validators import validate_quantities
 
 class HarrisSheet:
     r"""
-    Define a Harris Sheet Equilibrium.
-
-    Magnetic field will be in the :math:`±x` direction and the current
-    density will be in the :math:`±z` direction in a :math:`\hat{x} ×
-    \hat{y} = \hat{z}` coordinate system.
+    Representation of a 1D Harris sheet equilibrium.
 
     Parameters
     ----------
@@ -24,54 +20,70 @@ class HarrisSheet:
          units convertible to teslas.
 
     delta : `~astropy.units.Quantity`
-        The thickness of the current sheet in units convertible to
-        meters.
+        The characteristic thickness of the current sheet in units
+        convertible to meters.
 
-    P0 : `~astropy.units.Quantity`
+    p0 : `~astropy.units.Quantity`
         The plasma pressure in the limit of :math:`y → ∞` in units
         convertible to pascals.
 
     Notes
     -----
-    A current sheet is current limited to a surface.
+    A Harris sheet is a 1D ideal MHD equilibrium which is often used as
+    the initial condition for simulations of magnetic reconnection.
 
-    A Harris sheet is a 1D ideal MHD equilibrium. In resistive MHD if
-    there is any resistivity, it won't be a true equilibrium since the
-    resistivity will gradually smooth the profile out over time.
+    In this representation, the magnetic field will be in the :math:`±x`
+    direction and the current density will be in the :math:`±z`
+    direction in a :math:`\hat{x} × \hat{y} = \hat{z}`
+    coordinate system.
 
-    A Harris sheet is often used as the initial condition for
-    simulations of magnetic reconnection.
+    In resistive MHD if there is any resistivity, a Harris sheet will
+    not be a true equilibrium since the resistive diffusion will
+    gradually smooth out the magnetic profile over time.
 
     Examples
     --------
     >>> import astropy.units as u
-    >>> harris_sheet = HarrisSheet(delta = 3 * u.m, B0 = 2 * u.T)
-    >>> harris_sheet.magnetic_field(y = 5 * u.m)
-    <Quantity 1.8622... T>
+    >>> harris_sheet = HarrisSheet(delta = 1 * u.m, B0 = 1 * u.T, p0 = 0 * u.Pa)
+    >>> harris_sheet.magnetic_field(y = 1 * u.m)
+    <Quantity 0.761594... T>
+    >>> harris_sheet.current_density(y = 1 * u.m)
+    <Quantity -334204.96... A / m2>
+    >>> harris_sheet.plasma_pressure(y = 1 * u.m)
+    <Quantity 167102.48... Pa>
     """
 
-    def __init__(self, B0, delta, P0=0 * u.Pa):
+    @validate_quantities(p0={"can_be_negative": False})
+    def __init__(self, B0: u.T, delta: u.m, p0: u.Pa = 0 * u.Pa):
         self.B0 = B0
         self.delta = delta
-        self.P0 = P0
+        self.p0 = p0
 
     @validate_quantities
     def magnetic_field(self, y: u.m) -> u.T:
         r"""
-        Compute the magnetic field.
-
-        In this equation, :math:`B_0` is the asymptotic magnitude of the
-        magnetic field for :math:`y → ±∞` and :math:`δ` is the thickness
-        of the sheet.
-
-        .. math::
-
-            B_x(y) = B_0 \tanh \left( \frac{y}{δ} \right)
+        Compute the magnetic field strength.
 
         Parameters
         ----------
         y : `~astropy.units.Quantity`
            Orthogonal distance from the current sheet center.
+
+        Returns
+        -------
+        `~astropy.units.Quantity`
+
+        Notes
+        -----
+        The magnetic field of a Harris sheet is given by
+
+        .. math::
+
+            B_x(y) = B_0 \tanh \left( \frac{y}{δ} \right)
+
+        In this equation, :math:`B_0` is the asymptotic magnetic field
+        strength and :math:`δ` is the characteristic thickness of the
+        Harris sheet.
         """
         return self.B0 * np.tanh(u.rad * y / self.delta)
 
@@ -80,14 +92,26 @@ class HarrisSheet:
         r"""
         Compute the current density.
 
-        .. math::
-
-          J_z(y) = - \frac{B_0}{δ μ_0) \mathrm{sech}^2 \left( \frac{y}{δ} \right)
-
         Parameters
         ----------
         y : `~astropy.units.Quantity`
           Orthogonal distance from the current sheet center.
+
+        Returns
+        -------
+        `~astropy.units.Quantity`
+
+        Notes
+        -----
+        The current in a Harris sheet is given by
+
+        .. math::
+
+          J_z(y) = - \frac{B_0}{δ μ_0) \mathrm{sech}^2 \left( \frac{y}{δ} \right),
+
+        where :math:`B_0` is the asymptotic magnetic field strength,
+        :math:`δ` is the characteristic thickness of the Harris sheet,
+        and :math:`μ_0` is the vacuum magnetic permeability.
         """
         return (
             -self.B0 / (self.delta * const.mu0) * np.cosh(u.rad * y / self.delta) ** -2
@@ -96,18 +120,31 @@ class HarrisSheet:
     @validate_quantities
     def plasma_pressure(self, y: u.m) -> u.Pa:
         r"""
-        Compute plasma pressure.
-
-        .. math::
-
-            p(y) = \frac{B_0^2}{2 μ_0} \mathrm{sech}^2 \left( \frac{y}{δ} \right) + p_0
+        Compute the plasma pressure.
 
         Parameters
         ----------
         y : `~astropy.units.Quantity`
           Orthogonal distance from the current sheet center.
+
+        Returns
+        -------
+        `~astropy.units.Quantity`
+
+        Notes
+        -----
+        The plasma pressure in a Harris sheet is given by
+
+        .. math::
+
+            p(y) = \frac{B_0^2}{2 μ_0} \mathrm{sech}^2 \left( \frac{y}{δ} \right) + p_0,
+
+        where :math:`B_0` is the asymptotic magnetic field strength,
+        :math:`δ` is the characteristic thickness of the Harris sheet,
+        :math:`p_0` is the asymptotic plasma pressure, and :math:`μ_0`
+        is the vacuum magnetic permeability.
         """
         return (
             self.B0**2 / (2 * const.mu0) * (np.cosh(u.rad * y / self.delta) ** -2)
-            + self.P0
+            + self.p0
         )
--- plasmapy/plasma/tests/test_equilibria1d.py
+++ plasmapy/plasma/tests/test_equilibria1d.py
@@ -8,8 +8,8 @@ from plasmapy.plasma.equilibria1d import HarrisSheet
 def test_HarrisSheet():
     B0 = 1 * u.T
     delta = 1 * u.m
-    P0 = 0 * u.Pa
-    hs = HarrisSheet(B0, delta, P0)
+    p0 = 0 * u.Pa
+    hs = HarrisSheet(B0, delta, p0)
     B = hs.magnetic_field(0 * u.m)
     assert u.isclose(
         B, 0 * u.T, atol=1e-9 * u.T
@@ -19,8 +19,8 @@ def test_HarrisSheet():
 def test_HarrisSheet_pressure_balance():
     B0 = 1 * u.T
     delta = 1 * u.m
-    P0 = 0 * u.Pa
-    hs = HarrisSheet(B0, delta, P0)
+    p0 = 0 * u.Pa
+    hs = HarrisSheet(B0, delta, p0)
     y = [-7, -3, 0, 2, 47] * u.m
     B = hs.magnetic_field(y)
     P = hs.plasma_pressure(y)
@@ -32,8 +32,8 @@ def test_HarrisSheet_pressure_balance():
 def test_HarrisSheet_current_density():
     B0 = 1 * u.T
     delta = 1 * u.m
-    P0 = 0 * u.Pa
-    hs = HarrisSheet(B0, delta, P0)
+    p0 = 0 * u.Pa
+    hs = HarrisSheet(B0, delta, p0)
     y = [-2, 0, 2] * u.m
     J = hs.current_density(y)
     correct_J = [-56222.1400445, -795774.715459, -56222.1400445] * u.A / u.m**2
@@ -43,8 +43,8 @@ def test_HarrisSheet_current_density():
 def test_HarrisSheet_magnetic_field():
     B0 = 1 * u.T
     delta = 1 * u.m
-    P0 = 0 * u.Pa
-    hs = HarrisSheet(B0, delta, P0)
+    p0 = 0 * u.Pa
+    hs = HarrisSheet(B0, delta, p0)
     y = [-2, 0, 2] * u.m
     B = hs.magnetic_field(y)
     correct_B = [-0.96402758007, 0, 0.96402758007] * u.T
@@ -55,11 +55,11 @@ def test_HarrisSheet_limits():
     y = [-np.inf, np.inf] * u.m
     B0 = 1 * u.T
     delta = 1 * u.m
-    P0 = 0 * u.Pa
-    hs = HarrisSheet(B0, delta, P0)
+    p0 = 0 * u.Pa
+    hs = HarrisSheet(B0, delta, p0)
     B = hs.magnetic_field(y)
-    P = hs.plasma_pressure(y)
+    p = hs.plasma_pressure(y)
     J = hs.current_density(y)
     assert u.allclose(B, [-B0, B0], atol=1e-9 * u.T)
-    assert u.allclose(P, [P0, P0], atol=1e-9 * u.Pa)
+    assert u.allclose(p, [p0, p0], atol=1e-9 * u.Pa)
     assert u.allclose(J, [0, 0] * u.amp / u.m**2, atol=1e-9 * u.amp / u.m**2)
