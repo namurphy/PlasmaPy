37b65b564c7d95636976194695c2246bca6efe97
Nick Murphy <namurphy@cfa.harvard.edu>
2024-01-08 19:30:36 -0500

    Add plasmapy.analysis.multipoint

--- plasmapy/analysis/__init__.py
+++ plasmapy/analysis/__init__.py
@@ -1,5 +1,5 @@
 """The analysis subpackage for PlasmaPy."""
 
-__all__ = ["fit_functions", "nullpoint", "swept_langmuir"]
+__all__ = ["fit_functions", "multipoint", "nullpoint", "swept_langmuir"]
 
-from plasmapy.analysis import fit_functions, nullpoint, swept_langmuir
+from plasmapy.analysis import fit_functions, multipoint, nullpoint, swept_langmuir
